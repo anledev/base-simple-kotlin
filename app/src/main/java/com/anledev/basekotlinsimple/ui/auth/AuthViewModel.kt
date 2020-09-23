53f4b67210d40025a1d42800a0287a876864fd03
anlt <thanhan.le21@gmail.com>
2020-09-23 14:10:19 +0700

    User Login

--- app/src/main/java/com/anledev/basekotlinsimple/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/anledev/basekotlinsimple/ui/auth/AuthViewModel.kt
@@ -1,7 +1,21 @@
 package com.anledev.basekotlinsimple.ui.auth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anledev.basekotlinsimple.network.Resource
 import com.anledev.basekotlinsimple.repository.AuthRepository
+import com.anledev.basekotlinsimple.response.LoginResponse
+import kotlinx.coroutines.launch
 
 class AuthViewModel(private val repository: AuthRepository) : ViewModel() {
+
+    private val _loginResponse: MutableLiveData<Resource<LoginResponse>> = MutableLiveData()
+    val loginResponse: LiveData<Resource<LoginResponse>>
+        get() = _loginResponse
+
+    fun login(email: String, password: String) = viewModelScope.launch {
+        _loginResponse.value = repository.login(email, password)
+    }
 }
--- app/src/main/java/com/anledev/basekotlinsimple/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/anledev/basekotlinsimple/ui/auth/LoginFragment.kt
@@ -3,8 +3,11 @@ package com.anledev.basekotlinsimple.ui.auth
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.anledev.basekotlinsimple.databinding.FragmentLoginBinding
 import com.anledev.basekotlinsimple.network.AuthApi
+import com.anledev.basekotlinsimple.network.Resource
 import com.anledev.basekotlinsimple.repository.AuthRepository
 import com.anledev.basekotlinsimple.ui.base.BaseFragment
 
@@ -12,6 +15,24 @@ class LoginFragment : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepo
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        viewModel.loginResponse.observe(viewLifecycleOwner, Observer {
+            when(it){
+                is Resource.Success -> {
+                    Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_SHORT).show()
+                }
+                is Resource.Failure -> {
+                    Toast.makeText(requireContext(),"Login Failure", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.editTextTextEmailAddress.text.toString().trim()
+            val password = binding.editTextTextPassword.text.toString().trim()
+            //@todo add input validations
+            viewModel.login(email, password)
+        }
     }
 
     override fun getViewModel(): Class<AuthViewModel> = AuthViewModel::class.java
